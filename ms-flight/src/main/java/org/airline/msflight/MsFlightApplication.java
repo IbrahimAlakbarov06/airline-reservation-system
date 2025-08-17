61b0720121490db2a539c1fe63752e8da6325617
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-08-17 21:03:36 +0400

    ms-flight created

---
+++ ms-flight/src/main/java/org/airline/msflight/MsFlightApplication.java
@@ -0,0 +1,15 @@
+package org.airline.msflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsFlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsFlightApplication.class, args);
+    }
+
+}
