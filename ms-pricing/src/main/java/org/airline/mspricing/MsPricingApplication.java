42ddaabc3060e6343959d470d420346ec7c0d02b
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-09-04 18:37:38 +0400

    ms-pricing created

---
+++ ms-pricing/src/main/java/org/airline/mspricing/MsPricingApplication.java
@@ -0,0 +1,17 @@
+package org.airline.mspricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class MsPricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPricingApplication.class, args);
+	}
+
+}
---
+++ ms-pricing/src/test/java/org/airline/mspricing/MsPricingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.airline.mspricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPricingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
